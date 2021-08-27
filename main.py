89e8acc37e1211b95b12f1f3e8f495e1ea064363
user <davidmacarabrown@gmail.com>
2021-08-27 16:19:30 +0100

    working on write mode and memory register

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ led3 = machine.Pin(2, machine.Pin.OUT)
 led4 = machine.Pin(3, machine.Pin.OUT)
 led5 = machine.Pin(4, machine.Pin.OUT)
 
+writeLed = machine.Pin(5, machine.Pin.OUT)
+
+
 switch1 = machine.Pin(20, machine.Pin.IN, Pin.PULL_DOWN)
 switch2 = machine.Pin(19, machine.Pin.IN, Pin.PULL_DOWN)
 switch3 = machine.Pin(18, machine.Pin.IN, Pin.PULL_DOWN)
@@ -14,12 +17,15 @@ switch4 = machine.Pin(17, machine.Pin.IN, Pin.PULL_DOWN)
 switch5 = machine.Pin(16, machine.Pin.IN, Pin.PULL_DOWN)
 
 modeSwitch = machine.Pin(21, machine.Pin.IN, Pin.PULL_DOWN)
+writeSwitch = machine.Pin(22, machine.Pin.IN, Pin.PULL_DOWN)
 
-ledList = [led1, led2, led3, led4, led5]
+ledList = [led1, led2, led3, led4, led5, writeLed]
 
 buttonPad = 0.1
+writeEnableTime = 5
 
 mode = False
+writeMode = False
     
 def resetOutputs():
     for leds in ledList:
@@ -34,8 +40,10 @@ programRegister = {
                     "B" : [1, 2, 3],
                     "C" : [4, 5],
                     "D" : [1, 2],
-                    "E" : [5]
+                    "E" : []
                     }
+
+memoryRegister = []
 
 instructionRegister = []
 
@@ -44,7 +52,18 @@ def loadProgramToInstructionRegister(patch):
     for parameter in program:
         instructionRegister.append(parameter)
     
-
+def interruptWrite(pin):
+    global writeMode
+    writeSwitch.irq(handler = None)
+    time.sleep(writeEnableTime)
+    
+    if pin.value() == 1:
+        writeLed.toggle()
+        writeMode = not writeMode
+    
+    writeSwitch.irq(handler = interruptWrite)
+    print("Write Mode: ", writeMode)
+    
 def interruptMode(pin):
     global mode
     global instructionRegister
@@ -162,6 +181,7 @@ switch4.irq(trigger=machine.Pin.IRQ_RISING, handler=interruptFour)
 switch5.irq(trigger=machine.Pin.IRQ_RISING, handler=interruptFive)
 
 modeSwitch.irq(trigger=machine.Pin.IRQ_RISING, handler=interruptMode)
+writeSwitch.irq(trigger=machine.Pin.IRQ_RISING, handler=interruptWrite)
 
 def instructionHandler():
     print("Queued Instructions: ", instructionRegister)
