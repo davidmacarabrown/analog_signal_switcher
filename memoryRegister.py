a2af46cd22aec8867bbd6a48e6b2bb88c470b9e3
user <davidmacarabrown@gmail.com>
2021-08-27 21:54:54 +0100

    added object

---
+++ memoryRegister.py
@@ -0,0 +1,20 @@
+class Memory:
+    
+    def __init__(self):
+        self.contents = []
+        
+    def clearOne(instruction):
+        self.contents.remove(instruction)
+    
+    def clearAll(self):
+        self.contents.clear()
+    
+    def loadInstruction(self, instruction):
+        if self.contents.count(instruction) == 0:
+            self.contents.append(instruction)
+    
+    def loadProgram(self, instructionList):
+        self.clearAll()
+        for inst in instructionList:
+            self.contents.append(inst)
+
