654c45762dc83c8ea1898894d9d538597785560f
David Macara Brown <84098167+davidmacarabrown@users.noreply.github.com>
2021-10-09 15:39:36 +0100

    refactor: added pin definitions

--- relay_output.py
+++ relay_output.py
@@ -1,9 +1,15 @@
-from machine import Pin
+import machine
 
 class RelayOutput:
     
     def __init__(self):
-        self.outputs = {}
+        self.outputs = {
+                        1: machine.Pin(6, machine.Pin.OUT),
+                        2: machine.Pin(7, machine.Pin.OUT),
+                        3: machine.Pin(8, machine.Pin.OUT),
+                        4: machine.Pin(9, machine.Pin.OUT),
+                        5: machine.Pin(28, machine.Pin.OUT)
+                        }
         
     def latchSingle(self, relay):
         self.outputs[relay].toggle()
